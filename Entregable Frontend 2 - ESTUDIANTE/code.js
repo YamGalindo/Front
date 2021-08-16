c662bb964bee03a120b55a84a2206a757b5e5be7
Yam <yamgalindo7@gmail.com>
2021-08-16 09:53:15 -0500

    Borrando archivos repetidos y agregando carpeta de inicio Front3

---
+++ Entregable Frontend 2 - ESTUDIANTE/code.js
@@ -0,0 +1,62 @@
+let datosPersona = {
+  nombre: "",
+  edad: 0,
+  ciudad: "",
+  interesPorJs: "",
+};
+
+function obtenerDatosDelUsuario() {
+  /* --------------- PUNTO 1: Escribe tu codigo a partir de aqui --------------- */
+  
+
+
+
+}
+
+
+
+function renderizarDatosUsuario() {
+  /* ------------------- NO TOCAR NI ELIMINAR ESTA FUNCION. ------------------- */
+  obtenerDatosDelUsuario();
+  /* --------------- PUNTO 2: Escribe tu codigo a partir de aqui --------------- */
+  
+
+
+
+}
+
+
+/* ------------------------- NO MODIFICAR ESTE ARRAY ------------------------ */
+const listado = [
+  {
+    imgUrl: "https://huguidugui.files.wordpress.com/2015/03/html1.png",
+    lenguajes: "HTML y CSS",
+    bimestre: "1er bimestre",
+  },
+  {
+    imgUrl: "https://image.flaticon.com/icons/png/512/919/919828.png",
+    lenguajes: "Javascript",
+    bimestre: "2do bimestre",
+  },
+  {
+    imgUrl: "https://image.flaticon.com/icons/png/512/919/919851.png",
+    lenguajes: "React JS",
+    bimestre: "3er bimestre",
+  },
+];
+
+function recorrerListadoYRenderizarTarjetas() {
+  /* ------------------ PUNTO 3: Escribe tu codigo desde aqui ------------------ */
+  
+  
+
+
+}
+
+
+function mostrarYOcultarDescripcionCompleta() {
+  /* --------------------- PUNTO 4: Escribe tu codigo aqui --------------------- */
+ 
+  
+  
+}
